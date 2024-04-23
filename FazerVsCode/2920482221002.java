4728195358ba8b2f2898e895cc1b8426e1adea01
Stephanie Vieira <160241723+stephaniesvieira@users.noreply.github.com>
2024-04-23 11:49:34 -0300

    Update 2920482221002.java

--- FazerVsCode/2920482221002.java
+++ FazerVsCode/2920482221002.java
@@ -1,1 +1,6 @@
-
+Stephanie de Sousa Vieira; //Nome Completo
+2920482221002; //RGM
+25; //Idade
+@stephaniesvieira; //@ GitHub
+@Stephanie Vieira; //@ Linkedin
+Análise e Desenvolvimento de Sistemas, 4° semestre. //Turma e Semestre
