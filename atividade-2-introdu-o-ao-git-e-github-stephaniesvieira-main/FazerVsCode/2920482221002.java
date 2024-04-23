5b7b87ca0919c0bf6ad4ccee0343e8b7fdf205b4
stephaniesvieira <stephanievieira.dev@outlook.com>
2024-04-23 12:51:51 -0300

    Adicionar arquivo 2920482221002.java com detalhes pessoais

---
+++ atividade-2-introdu-o-ao-git-e-github-stephaniesvieira-main/FazerVsCode/2920482221002.java
@@ -0,0 +1,6 @@
+Stephanie de Sousa Vieira; \\Nome Completo
+2920482221002; \\RGM
+25; \\Idade
+@stephaniesvieira; \\Usuário GitHub
+@Stephanie Vieira; \\Usuário LinkedIn
+Análise e Desenvolvimento de Sistemas 4° semestre; \\Curso e Turma
